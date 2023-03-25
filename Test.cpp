85ae4da640d9329544bd1ec342154298d70446d8
yuvaltrip <yuvalbensimhon33@gmail.com>
2023-03-25 12:30:53 -0700

    Tweaks.

--- Test.cpp
+++ Test.cpp
@@ -27,13 +27,13 @@ TEST_CASE("Test about the start of the game")
     CHECK(p1.stacksize() == 26);//TRUE
     CHECK(p2.stacksize() == 26);//TRUE
 
-// Make sure that p1 and p2 have taken the expected amount of cards
+// Make sure that p1 and p2 have taken the expected amount of game_cards
     CHECK(p1.cardesTaken() + p2.cardesTaken() == 0);
 
-// Make sure there are in the start of the game still 52 cards
+// Make sure there are in the start of the game still 52 game_cards
     CHECK(p1.cardesTaken() + p2.cardesTaken() + p1.stacksize() + p2.stacksize() == 52);
 
-    CHECK_NOTHROW(game.printStats());// for each player prints basic statistics: win rate, cards won, <other stats you want to print>. Also print the draw rate and amount of draws that happand. (draw within a draw counts as 2 draws. )
+    CHECK_NOTHROW(game.printStats());// for each player prints basic statistics: win rate, game_cards won, <other stats you want to print>. Also print the draw rate and amount of draws that happand. (draw within a draw counts as 2 draws. )
     CHECK_NOTHROW(game.printLog());// prints all the turns played one line per turn (same format as game.printLastTurn())
 }
 TEST_CASE("Test about the middle of the game")
@@ -52,13 +52,13 @@ TEST_CASE("Test about the middle of the game")
     CHECK(p1.stacksize() == 21);//TRUE
     CHECK(p2.stacksize() == 21);//TRUE
 
-// Make sure that p1 and p2 have taken the expected amount of cards
+// Make sure that p1 and p2 have taken the expected amount of game_cards
     CHECK(p1.cardesTaken() + p2.cardesTaken() == 10);
 
-// Make sure there are in the middle of the game still 52 cards
+// Make sure there are in the middle of the game still 52 game_cards
     CHECK(p1.cardesTaken() + p2.cardesTaken() + p1.stacksize() + p2.stacksize() == 52);
 
-    CHECK_NOTHROW(game.printStats());// for each player prints basic statistics: win rate, cards won, <other stats you want to print>. Also print the draw rate and amount of draws that happand. (draw within a draw counts as 2 draws. )
+    CHECK_NOTHROW(game.printStats());// for each player prints basic statistics: win rate, game_cards won, <other stats you want to print>. Also print the draw rate and amount of draws that happand. (draw within a draw counts as 2 draws. )
     CHECK_NOTHROW(game.printLog());// prints all the turns played one line per turn (same format as game.printLastTurn())
 }
 TEST_CASE("Test about the end of the game")
@@ -82,15 +82,15 @@ CHECK_NOTHROW(game.playAll());
 CHECK(p1.stacksize()==0);//TRUE
 CHECK(p2.stacksize()==0);//TRUE
 
-// Make sure that p1 and p2 have taken the expected amount of cards
+// Make sure that p1 and p2 have taken the expected amount of game_cards
 CHECK(p1.cardesTaken()+ p2.cardesTaken() == 52);
 
-// Make sure there are in the end of the game still 52 cards
+// Make sure there are in the end of the game still 52 game_cards
 CHECK(p1.cardesTaken()+ p2.cardesTaken() + p1.stacksize() + p2.stacksize() == 52);
 
 //Check the function below are not falling
 CHECK_NOTHROW(game.printWiner());// prints the name of the winning player
-CHECK_NOTHROW(game.printStats());// for each player prints basic statistics: win rate, cards won, <other stats you want to print>. Also print the draw rate and amount of draws that happand. (draw within a draw counts as 2 draws. )
+CHECK_NOTHROW(game.printStats());// for each player prints basic statistics: win rate, game_cards won, <other stats you want to print>. Also print the draw rate and amount of draws that happand. (draw within a draw counts as 2 draws. )
 CHECK_NOTHROW(game.printLog());// prints all the turns played one line per turn (same format as game.printLastTurn())
 CHECK_NOTHROW(game.printLastTurn());
 
