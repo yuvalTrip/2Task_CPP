ae074a07aa9e2af9160ca56be04a8be474905522
yuvaltrip <yuvalbensimhon33@gmail.com>
2023-03-25 12:26:18 -0700

    player.cpp implementation finished.

--- sources/player.cpp
+++ sources/player.cpp
@@ -2,6 +2,7 @@
 // Created by yuvaltrip on 3/23/23.
 //
 # include "player.hpp"
+# include "game.hpp"
 # include <string>
 
 
@@ -11,26 +12,27 @@ Player::Player(std::string name)
     this->nameP=name;
 }
 
-
 string Player::getPName()
 // get the player name
 {
     return nameP;
 }
 
-//int Player::SideCards_player()
-//{
-//    return 1;
-//}
-
-int Player::stacksize() //return the amount of cards left. should be 21 but can be less if a draw was played
+int Player::stacksize() //return the amount of game_cards left. should be 21 but can be less if a draw was played
 {
-    return 1;
+    return numCardsLeft;
 }
 
-int Player::cardesTaken() // return the amount of cards this player has won.
-//prints the amount of cards player won and put in his side of the game.
+int Player::cardesTaken() // return the amount of game_cards this player has won.
+//prints the amount of game_cards player won and put in his side of the game.
 {
-    return 1;
+    return cards_won;
+}
+
+card_* Player::get_card()
+// Get card from the player card list (when it is player turn)
+{
+        numCardsLeft--;// for example in the start this number is 26 (so we will take the last element in index 25)
+        return (cardsLeft[numCardsLeft]);
 }
 
