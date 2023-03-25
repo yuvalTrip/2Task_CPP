889f0d45df4250ea21bd7070f619df834afc2a56
yuvaltrip <yuvalbensimhon33@gmail.com>
2023-03-25 12:27:02 -0700

    game.cpp implementation in process.

--- sources/game.cpp
+++ sources/game.cpp
@@ -12,22 +12,115 @@ Game::Game(Player first, Player second)
 {
     player1 = first;
     player2 = second;
-    card_* cards;//cardlist
+
 }
 
 void Game::initial_game()
-{// divided the cards randomly and equally between 2 players
+{
+    Game::game_status::NOT_STARTED_YET;
+// create list of 52 cards according to their shape and value
     for (int num = 1; num <= 13; num++) {
         for (int shape = 0; shape < 4; shape++) {
             card_::Shape cardShape = static_cast<card_::Shape>(shape);
             card_ card(num, cardShape);
-           // cards.addCard(card);
+            // Add Card to the game_cards list
+            game_cards[size_game_card++] = &card;
         }
     }
 }
 
+void Game::divide_cards()
+// divided the game_cards randomly and equally between 2 players
+{
+    // Randomize the order of the game_cards
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(game_cards, game_cards + CARD_CAPACITY, g);
+    // Divide the game cards between 2 players
+    for (int i=0;i<26;i++)
+    {
+        player1.cardsLeft[i] = game_cards[i];
+    }
+    for(int i=26; i<52;i++)
+    {
+        player2.cardsLeft[i] = game_cards[i];
+    }
+}
 void Game::playTurn()
 {
+    if (player1.numCardsLeft>0)
+    //player 1 and player 2 have the same number of cards , so if it will be bigger than 0 we will do
+        //as follow:
+    {
+        if (Game::game_status==P1LOOSE || Game::game_status==P1WON || Game::game_status==NOT_STARTED_YET)
+        {
+            card_ card_p1 = *player1.get_card();// player 1 put down card
+            card_ card_p2 = *player2.get_card();// player 2 put down card
+            // if player 1 is won
+            if (card_p1.getCardValue() > card_p2.getCardValue()) {
+                player1.cards_won+=2;//won both cards
+                player1.numOfWins++;// update for stat
+                game_status::P1WON;
+            } else if (card_p1.getCardValue() < card_p2.getCardValue())
+                // if player 2 is won
+            {
+                player2.cards_won+=2;//won both cards
+                player2.numOfWins++;// update for stat
+                game_status::P1LOOSE;
+            }
+                // if there is a tie
+            else {
+                game_status::TIE;
+                // Add 2 card to the cardsTie list of both players
+                player1.cardsTie[player1.index_tie]=card_p1;
+                player2.cardsTie[player2.index_tie]=card_p1;
+                player1.cardsTie[player1.index_tie++]=card_p2;
+                player2.cardsTie[player2.index_tie++]=card_p2;
+
+            }
+        }
+        else// If the stat is tie
+        {
+            //we will get one more card for each player
+            //and send it to cardsTie
+            card_ card_temp1= *player1.get_card();// player 1 put down card
+            player1.cardsTie[player1.index_tie++]=card_temp1;
+            card_ card_temp2 = *player2.get_card();// player 2 put down card
+            player2.cardsTie[player2.index_tie++]=card_temp2;
+            //we will get one more card for both players and check which is the winner
+            // This is the last card
+            card_ card_last1= *player1.get_card();// player 1 put down the last card
+            //player1.cardsTie[player1.index_tie++]=card_last1;
+            card_ card_last2= *player2.get_card();// player 2 put down the last card
+            //player2.cardsTie[player2.index_tie++]=card_last2;
+            // Now we will check the values
+            if (card_last1.getCardValue() > card_last2.getCardValue()) {
+                player1.cards_won+=2;//won both cards
+                player1.numOfWins++;// update for stat
+                game_status::P1WON;
+            } else if (card_last1.getCardValue() < card_last2.getCardValue())
+                // if player 2 is won
+            {
+                player2.cards_won+=2;//won both cards
+                player2.numOfWins++;// update for stat
+                game_status::P1LOOSE;
+            }
+                // if there is a tie
+            else {
+                game_status::TIE;
+                // Add 2 card to the cardsTie list of both players
+                player1.cardsTie[player1.index_tie]=card_last1;
+                player2.cardsTie[player2.index_tie]=card_last1;
+                player1.cardsTie[player1.index_tie++]=card_last2;
+                player2.cardsTie[player2.index_tie++]=card_last2;
+
+            }
+
+
+        }
+
+
+    }
 
 }
 void Game::printLastTurn()
@@ -55,7 +148,7 @@ void Game::printLog(){
 } // prints all the turns played one line per turn (same format as game.printLastTurn())
 
 void Game::printStats(){
-}// for each player prints basic statistics: win rate, cards won, <other stats you want to print>. Also print the draw rate and amount of draws that happand. (draw within a draw counts as 2 draws. )
+}// for each player prints basic statistics: win rate, game_cards won, <other stats you want to print>. Also print the draw rate and amount of draws that happand. (draw within a draw counts as 2 draws. )
 
 
 
--- sources/game.hpp
+++ sources/game.hpp
@@ -4,13 +4,17 @@
 
 #ifndef INC_2TASK_GAME_HPP
 #define INC_2TASK_GAME_HPP
-
+#define CARD_CAPACITY 52
 
 #include "iostream"
+#include <random>
+#include <algorithm>
 namespace ariel{}
 using namespace  ariel;
 #include <string>
 #include "player.hpp"
+#include "card.hpp"
+
 using namespace std;
 
 
@@ -19,24 +23,25 @@ class Game{
 private:
     Player player1;
     Player player2;
+    //card_* game_cards;//cardlist
 
 public:
     Game(){}; //empty constructor
     Game(Player p, Player t);
-    void initial_game(); // divided the cards randomly and equally between 2 players
+    enum game_status { P1WON, P1LOOSE, TIE, NOT_STARTED_YET };// The status in specific turn
+    void initial_game(); // create list of 52 cards according to their shape and value
+    void divide_cards();// divided the game_cards randomly and equally between 2 players
     void playTurn();
     void playAll(); //plays the game until the end
     void printWiner(); // prints the name of the winning player
     void printLog(); // prints all the turns played one line per turn (same format as game.printLastTurn())
-    void printStats();// for each player prints basic statistics: win rate, cards won, <other stats you want to print>. Also print the draw rate and amount of draws that happand. (draw within a draw counts as 2 draws. )
-
-
+    void printStats();// for each player prints basic statistics: win rate, game_cards won, <other stats you want to print>. Also print the draw rate and amount of draws that happand. (draw within a draw counts as 2 draws. )
     void printLastTurn();
     // print the last turn stats. For example:
     // Alice played Queen of Hearts Bob played 5 of Spades. Alice wins.
     // Alice played 6 of Hearts Bob played 6 of Spades. Draw. Alice played 10 of Clubs Bob played 10 of Diamonds. draw. Alice played Jack of Clubs Bob played King of Diamonds. Bob wins.
-
-
+    int size_game_card=0;//the index of card in the start when we put game_cards in game_cards last (in initialization game)
+    card_* game_cards[CARD_CAPACITY];//all the cards in the game
 
 };
 
